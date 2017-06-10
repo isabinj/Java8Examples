5102dda540e74d6742b39321bfb5363ec93607ac
Sabin Thomas <isabinj@gmail.com>
2017-06-10 17:03:45 +0530

    testing

--- Java8Examples/src/CollectionsStream.java
+++ Java8Examples/src/CollectionsStream.java
@@ -7,7 +7,8 @@ import java.util.stream.Stream;
 public class CollectionsStream {
 	public static void main(String... args){
 		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
-		
+
+
 		//-------------------------------------
 		List<Integer> even = list.stream().
 				filter( i -> i % 2 == 0)
